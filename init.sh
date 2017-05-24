57327f71a77f9ae6e885e0290e561ee537676842
philipperemy <premy@reactive.co.jp>
2017-05-24 15:14:46 +0900

    updated for python3

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 wget -nc http://nlp.stanford.edu/software/stanford-ner-2015-12-09.zip
 unzip stanford-ner-2015-12-09.zip
 mv stanford-ner-2015-12-09 stanford-ner
-echo "Google bought IBM for $10." > samples.txt
+echo "Google bought IBM for 10 dollars." > samples.txt
 python main.py -f samples.txt -v
 rm stanford-ner-2015-12-09.zip
