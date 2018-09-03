9c9ccea1262012855b805ba18cce217d740ea074
philipperemy <premy@cogent.co.jp>
2018-09-03 22:49:01 +0900

    Stanford NER ZIP (in case the proxy is down)

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-wget -nc http://nlp.stanford.edu/software/stanford-ner-2015-12-09.zip
+# MD5 (stanford-ner-2015-12-09.zip) = 18fb7a76a930e270eb261cbcfb993067
+# wget -nc http://nlp.stanford.edu/software/stanford-ner-2015-12-09.zip
+rm -rf stanford-ner-2015-12-09.zip # if any.
+cat stanford-ner-2015-12-09.zip* > stanford-ner-2015-12-09.zip
 unzip stanford-ner-2015-12-09.zip
 mv stanford-ner-2015-12-09 stanford-ner
 echo "Google bought IBM for 10 dollars. Mike was happy about this deal." > samples.txt
